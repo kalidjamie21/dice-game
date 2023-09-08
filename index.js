1744585aeab80ecef9af40d6aa2ca00c901a7838
Kalid Mawi <kalidjamie21@gmail.com>
2023-09-09 01:38:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ document.querySelector(".img1").setAttribute("src", images[randomNumber1]);
 document.querySelector(".img2").setAttribute("src", images[randomNumber2]);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML="Player 1 Wins!🤸‍♀️";
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML="Player 2 Wins!🤸‍♀️";
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
 } else {
     document.querySelector("h1").innerHTML="Draw!";
 }
